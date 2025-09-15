31ebbe75511f4bc3334321106e17ad8e2ef6f23e
Yash Gupta <guptayas9837@gmail.com>
2025-09-15 20:01:29 +0530

    making changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   // This enables CSS optimization which uses lightningcss
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
   // Optional: If you're using styled-components
   compiler: {
